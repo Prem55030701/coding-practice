6e0ccee3d39df6b3d784fa53b08410d495cb82c6
Prem Kumar <prem1016299@gmail.com>
2023-07-29 22:01:41 +0530

    updated code

--- src/main/java/NextGraterElement.java
+++ src/main/java/NextGraterElement.java
@@ -1,13 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 public class NextGraterElement {
     public static void main(String[] args) {
-        int arr[] = {13,7,6,12};
+        int arr[] = {3, 8, 4, 1, 2, 6, 7, 2};
         //printNGEBruteForce(arr);
-        printStack(arr);
+        //printStack(arr);
+        printOptimal(arr);
     }
 
+  static void printOptimal(int[] arr){
+       int i;
+       List<Integer> list=new ArrayList<>(Collections.nCopies(arr.length, -1));
+       int max=arr[arr.length-1];
+       for(i=arr.length-2;i>=0;i--)
+       {
+           if(arr[i+1]>arr[i])
+           {
+              list.set(i,arr[i+1]);
+           }
+          /* else {
+              // We do not need this , This condition is already covered in max logic
+               if (list.get(i+1)>arr[i])
+                   list.set(i,list.get(i+1)); */
+               else if (max>arr[i])
+               {
+                   int k=i+1;
+                   while (arr[k]<=arr[i])
+                       k++;
+                   list.set(i,arr[k]);
+               }
+          // }
 
+           max=Math.max(max,arr[i]);
+       }
+
+      for (int l = 0; l < arr.length; l++) {
+          System.out.println(arr[l]+"----->"+list.get(l));
+      }
+}
 
     static void printStack(int[] arr){
         Stack<Integer> stack=new Stack<>();
